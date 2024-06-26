b1342c98d0546a604e7410e11f182aea43a69e1e
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2024-06-26 12:14:25 -0400

    updated handler

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -1,44 +1,52 @@
-import React from "react";
-import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
+import RemoveBtn from "./RemoveBtn";
 
 const getTopics = async () => {
   try {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/topics`, {
-      // get an update data
       cache: "no-store",
     });
+
     if (!res.ok) {
       throw new Error("Failed to fetch topics");
     }
+
     return res.json();
-  } catch (err) {
-    console.log("Error fetching topics");
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+    return null; // Return null if there is an error
   }
 };
 
 export default async function TopicsList() {
-  const { topics } = await getTopics();
+  const result = await getTopics();
+
+  if (!result || !result.topics) {
+    return <div>Failed to load topics.</div>; // Display an error message if topics is undefined
+  }
+
+  const { topics } = result;
 
   return (
-    <div>
-      {topics.map((data, i) => (
+    <>
+      {topics.map((t) => (
         <div
+          key={t._id}
           className="flex items-start justify-between gap-5 p-4 my-3 border border-slate-300"
-          key={i}
         >
           <div>
-            <h2 className="text-2xl font-bold">{data.title}</h2>
-            <div>{data.description}</div>
+            <h2 className="text-2xl font-bold">{t.title}</h2>
+            <div>{t.description}</div>
           </div>
-          <div className="flex gap-2 ">
-            <RemoveBtn id={data._id} />
-            <Link href={`/editTopic/${data._id}`}>
+
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
               <div>Edit</div>
             </Link>
           </div>
         </div>
       ))}
-    </div>
+    </>
   );
 }
